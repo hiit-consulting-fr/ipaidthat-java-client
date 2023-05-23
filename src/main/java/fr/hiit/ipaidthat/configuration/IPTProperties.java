41279de1c0e673196f1594196e8da5f4b843019a
Michaël COLL <michael.coll@hiit-consulting.fr>
2023-05-23 21:20:01 +0200

    chore: switch to opensource
    - spring boot version upgrade
    - add dependabot auto merge
    - add commit-lint
    - dependency version upgrade

--- src/main/java/fr/hiit/ipaidthat/configuration/IPTProperties.java
+++ src/main/java/fr/hiit/ipaidthat/configuration/IPTProperties.java
@@ -1,12 +1,11 @@
 package fr.hiit.ipaidthat.configuration;
 
 import feign.Logger;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.annotation.Validated;
-
-import javax.validation.constraints.NotNull;
 
 @Data
 @Validated
--- src/main/java/fr/hiit/ipaidthat/proxy/model/InvoiceSimpleItem.java
+++ src/main/java/fr/hiit/ipaidthat/proxy/model/InvoiceSimpleItem.java
@@ -1,20 +1,19 @@
 package fr.hiit.ipaidthat.proxy.model;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class InvoiceSimpleItem {
 
     private Long id;
